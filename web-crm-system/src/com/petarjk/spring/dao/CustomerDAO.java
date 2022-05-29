3df19542de369301a635a4ce366be24c69dc032c
Petar Yankov <petarroma@gmail.com>
2022-05-29 19:12:23 +0300

    Initial commit

---
+++ web-crm-system/src/com/petarjk/spring/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.petarjk.spring.dao;
+
+import java.util.List;
+
+import com.petarjk.spring.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
