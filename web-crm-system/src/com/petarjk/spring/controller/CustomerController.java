dabbba30e6086ef50cf9c969c04fbe8af0736789
Petar Yankov <petarroma@gmail.com>
2022-06-04 19:52:00 +0300

    Add service layer between the controller and the DAO

--- web-crm-system/src/com/petarjk/spring/controller/CustomerController.java
+++ web-crm-system/src/com/petarjk/spring/controller/CustomerController.java
@@ -5,22 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.petarjk.spring.dao.CustomerDAO;
 import com.petarjk.spring.entity.Customer;
+import com.petarjk.spring.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		List<Customer> theCustomers = customerService.getCustomers();
 
 		theModel.addAttribute("customers", theCustomers);
 
